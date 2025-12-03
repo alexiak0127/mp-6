fbd87cd4051d267020cc943197b9cd16f7fea110
Hayeon(Alexia) Kim <alexiak@bu.edu>
2025-12-02 22:45:23 -0500

    error fixed

--- auth.ts
+++ auth.ts
@@ -1,14 +1,17 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 
-export const {
-  handlers,
-  auth,
-} = NextAuth({
+export const { auth, handlers, signIn, signOut } = NextAuth({
   providers: [
     GitHub({
       clientId: process.env.AUTH_GITHUB_ID!,
       clientSecret: process.env.AUTH_GITHUB_SECRET!,
+      authorization: {
+        params: {
+          scope: "read:user user:email",
+          prompt: "consent",
+        },
+      },
     }),
   ],
 });
